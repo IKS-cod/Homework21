bc6b0f288c40b94e7b2711bbe4c180c01d949405
IKS-cod <ik.smirnov@yandex.ru>
2024-05-07 20:54:36 +0300

    Initial commit

---
+++ Homework21/src/test/java/Homework21/Homework21ApplicationTests.java
@@ -0,0 +1,13 @@
+package Homework21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
