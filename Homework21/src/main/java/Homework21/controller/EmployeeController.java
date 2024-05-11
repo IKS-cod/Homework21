a39ae98d48d33708272e7078486b19b246fcdfb4
IKS-cod <ik.smirnov@yandex.ru>
2024-05-11 21:16:00 +0300

    ДЗ 21

---
+++ Homework21/src/main/java/Homework21/controller/EmployeeController.java
@@ -0,0 +1,52 @@
+package Homework21.controller;
+
+import Homework21.model.Employee;
+import Homework21.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public Collection<Employee> findAll() {
+        return employeeService.findAll();
+    }
+
+
+
+    @GetMapping("/add")
+    public Employee addEmployee(@RequestParam String firstName,
+                                @RequestParam String lastName,
+                                @RequestParam Integer departmentId,
+                                @RequestParam Double salary) {
+        return employeeService.addEmployee(firstName, lastName, departmentId, salary);
+    }
+
+    @GetMapping("/remove")
+    public Employee removeEmployee(@RequestParam String firstName,
+                                   @RequestParam String lastName) {
+        return employeeService.removeEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/find")
+    public Employee findEmployee(@RequestParam String firstName,
+                                 @RequestParam String lastName) {
+        return employeeService.findEmployee(firstName, lastName);
+    }
+
+
+}
